7f716756007982540329cba4959a10b123adfc33
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2021-12-04 19:29:29 -0300

    Instalacion y configuracion de Socket.io

--- models/server.js
+++ models/server.js
@@ -5,6 +5,8 @@ class Server {
   constructor() {
     this.app = express()
     this.port = process.env.PORT
+    this.server = require('http').createServer(this.app)
+    this.io = require('socket.io')(this.server)
 
     // Middlewares
     this.middlewares()
@@ -24,7 +26,7 @@ class Server {
   routes() {}
 
   listen() {
-    this.app.listen(this.port, () => {
+    this.server.listen(this.port, () => {
       console.log('Servidor corriendo en puerto', this.port)
     })
   }
